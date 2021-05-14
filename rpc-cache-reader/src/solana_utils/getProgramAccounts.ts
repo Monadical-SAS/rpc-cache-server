172f1368aed0699ea4ee3cecd5aaa7e35dc454fc
Thomas Lisankie <id@tomlisankie.com>
2021-05-14 16:53:46 -0500

    Fixed bugs in the reader that were mainly related to the Lambda environment specifically

--- rpc-cache-reader/src/solana_utils/getProgramAccounts.ts
+++ rpc-cache-reader/src/solana_utils/getProgramAccounts.ts
@@ -5,89 +5,96 @@ import bs58 from "bs58";
 import util from "util";
 
 export const getProgramAccounts = async (
-  params: Partial<JSONRPCParams> | undefined
+    params: Partial<JSONRPCParams> | undefined
 ): Promise<Array<any>> => {
     console.log("In getProgramAccounts function in solana_utils/getProgramAccounts.ts in read");
-  return new Promise((resolve, reject) => {
-    if (params) {
-      const programID = (params as any[])[0];
-      const filters = (params as any[])[1];
-      redisClient.hvals(programID, function (err, reply) {
-        if (!reply || reply.length === 0 || err) {
-          if (err) console.log(err);
-          reject(err);
+    return new Promise((resolve, reject) => {
+        if (params) {
+            console.log("There are parameters");
+            const programID = (params as any[])[0];
+            const filters = (params as any[])[1];
+            redisClient.hvals(programID, function (err, reply) {
+                console.log("Ran hvals");
+                if (!reply || reply.length === 0 || err) {
+                    console.log("There was no reply");
+                    if (err) console.log(err);
+                    reject(err);
+                } else {
+                    const parsed: Array<ParsedKeyedAccountInfo> = reply.map((acc) =>
+                        JSON.parse(acc)
+                                                                           );
+                    console.log("resolving the getProgramAccounts Promise");
+                    resolve(filterProgramAccounts(parsed, filters, programID));
+                }
+            });
+            console.log("Past hvals being called");
         } else {
-          const parsed: Array<ParsedKeyedAccountInfo> = reply.map((acc) =>
-            JSON.parse(acc)
-          );
-          resolve(filterProgramAccounts(parsed, filters, programID));
+            console.log("No parameters");
+            reject("no parameters");
         }
-      });
-    } else {
-      reject("no parameters");
-    }
-  });
+    });
 };
 
 const filterProgramAccounts = (
-  accounts: Array<ParsedKeyedAccountInfo>,
-  config: Record<string, unknown>,
-  programID: string
+    accounts: Array<ParsedKeyedAccountInfo>,
+    config: Record<string, unknown>,
+    programID: string
 ): Array<ParsedKeyedAccountInfo> => {
-  console.log(util.inspect(config, { showHidden: false, depth: null }));
-  const filters = config.filters || [];
-  const encoding = config.encoding || "base58";
-  delete config.filters;
-  const filteredAccounts = accounts.filter((acc) => {
-    if (acc.account.owner !== programID) {
-      return false;
-    }
-    const decodedData = bs58.decode(acc.account.data as string);
-    acc.account.data = decodedData;
-    for (const filter of filters as any[]) {
-      const dataSize = filter.dataSize;
-      const memcmp = filter.memcmp;
-      const memcmpData = {
-        offset: memcmp?.offset,
-        bytes: memcmp?.bytes,
-      };
-      if (dataSize && !(decodedData.length === dataSize)) {
-        return false;
-      }
-      if (memcmpData.offset !== undefined && memcmpData.bytes !== undefined) {
-        let dBytes;
-        const dataArray = decodedData;
-        try {
-          dBytes = bs58.decode(memcmpData.bytes);
-        } catch {
-          return false;
+    console.log(util.inspect(config, { showHidden: false, depth: null }));
+    console.log("Filtering program accounts");
+    const filters = config.filters || [];
+    const encoding = config.encoding || "base58";
+    delete config.filters;
+    const filteredAccounts = accounts.filter((acc) => {
+        if (acc.account.owner !== programID) {
+            return false;
         }
-        if (memcmpData.offset > decodedData.length) {
-          return false;
+        const decodedData = bs58.decode(acc.account.data as string);
+        acc.account.data = decodedData;
+        for (const filter of filters as any[]) {
+            const dataSize = filter.dataSize;
+            const memcmp = filter.memcmp;
+            const memcmpData = {
+                offset: memcmp?.offset,
+                bytes: memcmp?.bytes,
+            };
+            if (dataSize && !(decodedData.length === dataSize)) {
+                return false;
+            }
+            if (memcmpData.offset !== undefined && memcmpData.bytes !== undefined) {
+                let dBytes;
+                const dataArray = decodedData;
+                try {
+                    dBytes = bs58.decode(memcmpData.bytes);
+                } catch {
+                    return false;
+                }
+                if (memcmpData.offset > decodedData.length) {
+                    return false;
+                }
+                if (dataArray.slice(memcmpData.offset).length < dBytes.length) {
+                    return false;
+                }
+                return (
+                    dataArray.compare(
+                        dBytes,
+                        0,
+                        dBytes.length,
+                        memcmpData.offset,
+                        memcmpData.offset + dBytes.length
+                    ) === 0
+                );
+            }
         }
-        if (dataArray.slice(memcmpData.offset).length < dBytes.length) {
-          return false;
+        return true;
+    });
+    return filteredAccounts.map((acc) => {
+        const decodedData = acc.account.data;
+        if (encoding === "base64") {
+            acc.account.data = (decodedData as Buffer).toString("base64");
+        } else {
+            acc.account.data = bs58.encode(decodedData as Buffer);
         }
-        return (
-          dataArray.compare(
-            dBytes,
-            0,
-            dBytes.length,
-            memcmpData.offset,
-            memcmpData.offset + dBytes.length
-          ) === 0
-        );
-      }
-    }
-    return true;
-  });
-  return filteredAccounts.map((acc) => {
-    const decodedData = acc.account.data;
-    if (encoding === "base64") {
-      acc.account.data = (decodedData as Buffer).toString("base64");
-    } else {
-      acc.account.data = bs58.encode(decodedData as Buffer);
-    }
-    return acc;
-  });
+        return acc;
+    });
 };
