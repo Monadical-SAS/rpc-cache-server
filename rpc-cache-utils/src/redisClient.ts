c27294096c90821e59585881827aee17b92aa88d
Thomas Lisankie <id@tomlisankie.com>
2021-05-14 12:48:21 -0500

    Changed rediss to https. Good grief!

--- rpc-cache-utils/src/redisClient.ts
+++ rpc-cache-utils/src/redisClient.ts
@@ -3,7 +3,7 @@ import redis from "redis";
 export const getRedisClient = (): redis.RedisClient => {
   if (process.env.ENV === "AWS") {
     return redis.createClient(
-      `rediss://${process.env.REDIS_SERVER_URL}:${process.env.REDIS_SERVER_PORT}`
+      `https://${process.env.REDIS_SERVER_URL}:${process.env.REDIS_SERVER_PORT}`
     );
   } else {
     return redis.createClient();
