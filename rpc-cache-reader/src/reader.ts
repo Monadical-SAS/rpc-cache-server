91ba7abf321f8db9300e81c254f270db7ddc9109
Thomas Lisankie <id@tomlisankie.com>
2021-09-13 10:25:03 -0500

    Reform flow for incoming JSON-RPC call

--- rpc-cache-reader/src/reader.ts
+++ rpc-cache-reader/src/reader.ts
@@ -6,6 +6,7 @@ import { connection } from "../../rpc-cache-utils/src/connection";
 import { getProgramAccounts } from "./solana_utils/getProgramAccounts";
 import { settings } from "../../rpc-cache-utils/src/config";
 import * as util from "util";
+import { JSONRPCRequest } from "json-rpc-2.0";
 
 const server = new JSONRPCServer();
 
@@ -13,12 +14,12 @@ const app = express();
 app.use(bodyParser.json() as RequestHandler);
 app.use(cors());
 
-let seenMethodNames = new Set();
+let seenRPCs = new Set<[any, any]>();
 
 for (const name of settings.cacheFunctions.names) {
   switch (name) {
     case "getProgramAccounts": {
-      seenMethodNames.add(name);
+      seenRPCs.add([name, ""]);
       server.addMethod("getProgramAccounts", getProgramAccounts);
       break;
     }
@@ -28,44 +29,21 @@ for (const name of settings.cacheFunctions.names) {
 }
 
 app.post("/", (req, res) => {
-  const jsonRPCRequest = req.body;
+  const jsonRPCRequest = req.body as JSONRPCRequest;
   // server.receive takes a JSON-RPC request and returns a Promise of a JSON-RPC response.
   console.log("received request", jsonRPCRequest.method, jsonRPCRequest.params);
-  if (seenMethodNames.has(jsonRPCRequest.method)) {
-    console.log("RPC method found in the config file");
-    server.receive(jsonRPCRequest).then((jsonRPCResponse) => {
-      // TODO: Add ability to handle cache misses for methods that are already in seenMethodNames.
-      if (jsonRPCResponse && jsonRPCResponse.error) {
-        console.log(
-          "rejected: " + util.inspect(jsonRPCResponse.error, { depth: null })
-        );
-        (connection as any)
-          ._rpcRequest(jsonRPCRequest.method, jsonRPCRequest.params)
-          .catch((e: any) => {
-            jsonRPCResponse.error = e;
-            res.json(jsonRPCResponse);
-          })
-          .then((resp: JSONRPCResponse) => {
-            res.json(resp);
-          });
-      } else if (jsonRPCResponse && !jsonRPCResponse.error) {
-        res.json(jsonRPCResponse);
-      } else {
-        res.sendStatus(204);
-      }
-    });
+  if (seenRPCs.has([jsonRPCRequest.method, jsonRPCRequest.params])) {
+    console.log("This RPC has been seen.");
+    res.json(getCachedValue(jsonRPCRequest)); 
   } else {
-    console.log("not handled");
-    // Add handler function so that it gets handled in the future
-    seenMethodNames.add(jsonRPCRequest.method);
-    server.addMethod(
-      jsonRPCRequest.method,
-      genericSolanaRPCHandler(jsonRPCRequest, res)
-    );
+    console.log("This RPC has not been seen yet. Going to cache now.");
+    seenRPCs.add([jsonRPCRequest.method, jsonRPCRequest.params]);
+    const writerResponse = askWriterForValue(jsonRPCRequest);
+    res.json(writerResponse);
   }
 });
 
-function genericSolanaRPCHandler(jsonRPCRequest: any, res: any) {
+function genericSolanaRPCHandler(jsonRPCRequest: JSONRPCRequest, res: any) {
   return async (params: Partial<JSONRPCParams> | undefined) => {
     return new Promise((resolve, reject) => {
       if (params) {
